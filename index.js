70baf49d50c309ea38adf4727a8da742c31be654
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-10-30 12:41:41 +0530

    start fronted

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
+const path = require("path");
 const { connectTomongoDb } = require("./connect");
-
+const URL = require("./models/url");
 const app = express();
 const PORT = 3001;
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get("/test", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home", { urls: allUrls });
+});
 
 connectTomongoDb("mongodb://localhost:27017/urlshortner")
   .then(() => {
@@ -16,7 +25,7 @@ connectTomongoDb("mongodb://localhost:27017/urlshortner")
 app.use(express.json());
 
 app.use("/url", urlRoute);
-app.get("/:shortId", urlRoute);
+app.get("/url/:shortId", urlRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
