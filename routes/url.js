a6f117c3d1903c5e71ac7f526b7190d1598fd9b3
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-10-31 19:27:03 +0530

    route update

--- routes/url.js
+++ routes/url.js
@@ -6,13 +6,13 @@ const {
 } = require("../controllers/url");
 const router = express.Router();
 
-
-
 // for home route //
 router.post("/", handleGenNewShortUrl);
 
 // for short id route //
 router.get("/:shortId", handleGetByShortIdAndUpdate);
+
+// for analytics route //
 router.get("/analytics/:shortId", handleGetAnalytics);
 
 module.exports = router;
