233dd0eca99f1b754017b135d0c2d31ec40cb0b4
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-11-03 15:27:08 +0530

    update controllers

--- controllers/url.js
+++ controllers/url.js
@@ -4,11 +4,10 @@ const URL = require("../models/url");
 async function handleGenNewShortUrl(req, res) {
   const body = req.body;
   if (!body.url) return res.status(400).json({ error: "URL is required" });
-  const shortId = shortid.generate(); 
+  const shortId = shortid.generate();
   await URL.create({ shortId, redirectUrl: body.url, visitHistory: [] });
   return res.json({ id: shortId });
 }
-
 async function handleGetByShortIdAndUpdate(req, res) {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
