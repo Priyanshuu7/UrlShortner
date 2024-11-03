89dc866e42269b782f2e13dd8466ced5e38279bf
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-11-03 15:05:11 +0530

    update connect.js

--- connect.js
+++ connect.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
-
 // code for mongodb //
 async function connectTomongoDb(url) {
   return mongoose.connect(url);
 }
-
 module.exports = { connectTomongoDb };
